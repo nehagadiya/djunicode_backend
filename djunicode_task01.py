7c5bf4d14855edffa5c6b96bd4071a0783d287fb
nehagadiya <67506947+nehagadiya@users.noreply.github.com>
2020-08-12 11:39:21 +0530

    Added comments

--- djunicode_task01.py
+++ djunicode_task01.py
@@ -1,15 +1,18 @@
 def check (first,last):
-    present = {}
+    present = {} 
     for i in range (first, last):
+        #initializing necessary variables for each iteration
         temp = 1
         b = 0
         t = i
+        #creating binary value of decimal number using while loop
         while i != 0:
             r = i % 2
             i = i//2
             b = b + r*temp
             temp = temp*10
         present[t]=b
+    #checking if two consecutive 1 is present in the binary value
     for x in present:
         bs=str(present[x])
         if '11' in bs:
@@ -17,6 +20,7 @@ def check (first,last):
         else:
             present[x] = False
     print(present)
+    
 takein = input().split(',')
 check(int(takein[0]),int(takein[1]))
 
